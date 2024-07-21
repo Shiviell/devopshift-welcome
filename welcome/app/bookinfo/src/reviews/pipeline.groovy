764930b80d9c76ecb345459e6636cfea9a8ac3c6
Shiviell <107198510+Shiviell@users.noreply.github.com>
2024-07-21 17:37:40 +0300

    Create pipeline.groovy

---
+++ welcome/app/bookinfo/src/reviews/pipeline.groovy
@@ -0,0 +1,83 @@
+pipeline {
+    agent any
+
+    parameters {
+       string( name : 'gitrepo' , defaultValue :  "https://github.com/Shiviell/devopshift-welcome.git" ) 
+        //dockerCredentials = 'your-docker-credentials-id'  
+       string( name: 'imageName' , defaultValue : "reviews")  // Replace with your Docker image name
+        string (name : 'BUILD_NUMBER', defaultValue : "${env.BUILD_NUMBER}")
+        string (name : 'token', defaultValue : "123")
+        string(name : 'branch', defaultValue : "jenkins-workshop")
+        string(name : 'dockerfile', defaultValue : "devopshift-welcome/welcome/app/bookinfo/src/reviews" ) 
+    }
+    stages {
+        stage('Checkout') {
+            steps {
+                // Checkout code from Git repository
+                sh "git clone -b ${branch} ${gitrepo}"
+            }
+        }
+
+        stage('Build') {
+            steps {
+                script {
+                    // Build Docker image
+                    sh "pwd"
+                    //sh "cd ${dockerfile}"
+                   
+                    sh "docker build -f ${dockerfile}/Dockerfile -t shivi2021/${imageName}:1.0.${BUILD_NUMBER} ${dockerfile}"
+                    }
+                }
+            }
+        
+
+        stage('lint') {
+            steps {
+                script {
+                    // Build Docker image
+                    sh "pwd"
+                    //sh "cd ${dockerfile}"
+                   
+                    sh "docker build -f ${dockerfile}/Dockerfile -t shivi2021/${imageName}:1.0.${BUILD_NUMBER} ${dockerfile}"
+                    }
+                }
+            }
+        
+        stage('security') {
+            steps {
+                script {
+                // Run sectests inside the Docker container
+                sh "docker build -f ${dockerfile}/Dockerfile --build-arg SNYK_TOKEN=${token} -t shivi2021/${imageName}:1.0.${BUILD_NUMBER}-security ${dockerfile}"
+                sh "docker run -it -d -p 9080:9080 --name ${imageName}-sec shivi2021/${imageName}:1.0.${BUILD_NUMBER}-security" // docker run -it -d -p 9080:9080 --name productpage shivi2021/productpage:1.0.5
+                sh "docker ps"
+                sh "docker stop ${imageName}-sec"
+                sh "docker rm ${imageName}-sec"
+                }
+            }
+        }
+
+        stage('Test') {
+            steps {
+                script {
+                // Run tests inside the Docker container
+                sh "docker run -it -d -p 9080:9080 --name ${imageName} shivi2021/${imageName}:1.0.${BUILD_NUMBER}" // docker run -it -d -p 9080:9080 --name productpage shivi2021/productpage:1.0.5
+                sh "docker ps"
+                sh "docker stop ${imageName}"
+                sh "docker rm ${imageName}"
+                }
+            }
+        }
+
+        stage('Push') {
+            steps {
+                script {
+                    // Push Docker image to registry
+                    withCredentials([usernamePassword(credentialsId: 'dockerHub', passwordVariable: 'dockerHubPassword', usernameVariable: 'dockerHubUser')]) {
+                    sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPassword}"
+                    sh "docker push shivi2021/${imageName}:1.0.${BUILD_NUMBER}"
+                    }
+                }
+            }
+        }
+    }
+}
